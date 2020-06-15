2a95b9253a6f88a08fd80241c62625bb9d98d556
Nicolas Essayan <nicolas_essayan@yahoo.fr>
2020-06-15 14:34:54 +0200

    Solved

--- nico_project/lib.py
+++ nico_project/lib.py
@@ -48,6 +48,9 @@ def clean_data(data):
     return data
 
 
+def try_me():
+    print("c'est le package de Nico")
+
 if __name__ == '__main__':
     # For introspections purpose to quickly get this functions on ipython
     import nico_project
